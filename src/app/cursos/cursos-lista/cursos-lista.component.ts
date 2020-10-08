0184fd83c482690638d5700c00553290fa4a6282
Amanda Xavier <amandaxaviermariano10@gmail.com>
2020-10-08 12:24:40 -0300

    Pipe Async e HTTP GET

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CursosService } from '../cursos.service';
 import { Curso } from '../curso';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -10,10 +11,13 @@ import { Curso } from '../curso';
 })
 export class CursosListaComponent implements OnInit {
   cursos: Curso[];
+  cursos$: Observable<Curso[]>;//$ é um observable
   constructor(private service: CursosService) { }
 
   ngOnInit(): void {
-    this.service.list().subscribe(dados => this.cursos = dados);//se inscrever e ficar escutando as mudanças
+   // this.service.list().subscribe(dados => this.cursos = dados);//se inscrever e ficar escutando as mudanças
+ //de uma maneira mais prática
+    this.cursos$ = this.service.list();//para utilizar o async
   }
 
 }
